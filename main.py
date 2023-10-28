f9cbdb3f8a09fb015f4e1f612901919ae95d7e70
Marius20031 <49350853+Marius20031@users.noreply.github.com>
2023-10-28 14:32:08 +0300

    Add files via upload
    inceput de proiect all good so far

--- main.py
+++ main.py
@@ -12,16 +12,21 @@ Fundal = (44,62,80)
 WHITE = (255, 255, 255)
 
 # Set the dimensions of the board
-board_width, board_height = 600, 600
-board_x, board_y = (width - board_width) // 4, (height - board_height) // 2
+board_width, board_height = 400, 400
+board_x, board_y = (width - board_width) // 10, (height - board_height) // 2
 cell_size = board_width // 10
 
 # Set the dimensions of the second board
-board2_width, board2_height = 600, 600
-board2_x, board2_y = (width - board2_width) // 4 * 3, (height - board2_height) // 2
+board2_width, board2_height = 400, 400
+board2_x, board2_y = (width - board2_width) // 4 , (height - board2_height) // 2
 cell2_size = board2_width // 10
 
+#TIMMER
+import numpy as np
+
+
 # Draw the board
+
 def draw_board():
     for i in range(11):
         pygame.draw.line(screen, WHITE, (board_x + i * cell_size, board_y),
@@ -35,23 +40,72 @@ def draw_board():
         pygame.draw.line(screen, WHITE, (board2_x, board2_y + i * cell2_size),
                          (board2_x + board2_width, board2_y + i * cell2_size), 2)
 
-# Function to move the second board
+# Function to move
+#the second board
 def move_board2():
+    #move_board = True
+    #if move_board==False :
     global board2_x
-    board2_x += cell2_size
-
+    board2_x +=  cell2_size*200
+    #move_board=True
 # Main function to run the game
+global zz
+
+import pygame
+import sys
+
+
+
+# Set up the display
+#screen = pygame.display.set_mode((400, 300))
+#pygame.display.set_caption("Timer Example")
+
+# Initialize font
+
+# Function to display the timer
+def show_timer(seconds):
+    font = pygame.font.Font(None, 75) # FONTUL TIMMERULUI
+    screen.fill((255, 255, 255))
+    text = font.render(str(seconds), True, (0, 0, 0))
+    text_rect = text.get_rect(center=(450, 450))
+    screen.blit(text, text_rect)
+    pygame.display.flip()
+
+# Timer function
+def timer():
+    for i in range(3, 0, -1):
+        show_timer(i)
+        pygame.time.wait(1000)
+
+    # Call your function here after the timer is finished
+# Your function to be called after the timer
+def your_function():
+    # Your function logic goes here
+    print("Your function is called after the timer.")
+
+# Call the timer function
+
 def run_game():
+    timer()
+    zz = False
     running = True
+    # if zz==False :
+    #draw_board()  # Draw the board
+    initial_state=True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
-        screen.fill(Fundal)  # Fill the screen with Fundal
-        draw_board()  # Draw the boards
-        draw_board()  # Draw the boards
-        #move_board2()  # Move the second board
+                # conditie de iesire tre in while sa ii zic daca apesi pe x sau cv puton whatever si le fac butoane casutele
+            if initial_state==True:
+                print("a-")
+                screen.fill(Fundal)  # Fill the screen with Fundal
+                draw_board()  # Draw the boards
+                move_board2()  # Move the second board
+                pygame.display.flip()  # Update the full display Surface to the screen
+                zz = True
+                initial_state=False
+                # de revazut de ce nu se modifica
         pygame.display.flip()  # Update the full display Surface to the screen
 
     pygame.quit()
