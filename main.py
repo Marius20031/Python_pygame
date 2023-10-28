b421a39bce9a97a417f719ddc73df3dad139c978
Marius20031 <49350853+Marius20031@users.noreply.github.com>
2023-10-28 18:54:51 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -21,12 +21,7 @@ board2_width, board2_height = 400, 400
 board2_x, board2_y = (width - board2_width) // 4 , (height - board2_height) // 2
 cell2_size = board2_width // 10
 
-#TIMMER
-import numpy as np
-
-
 # Draw the board
-
 def draw_board():
     for i in range(11):
         pygame.draw.line(screen, WHITE, (board_x + i * cell_size, board_y),
@@ -40,51 +35,7 @@ def draw_board():
         pygame.draw.line(screen, WHITE, (board2_x, board2_y + i * cell2_size),
                          (board2_x + board2_width, board2_y + i * cell2_size), 2)
 
-# Function to move
-#the second board
-def move_board2():
-    #move_board = True
-    #if move_board==False :
-    global board2_x
-    board2_x +=  15*(cell2_size-15)
-    #move_board=True
-# Main function to run the game
-global zz
-
-import pygame
-import sys
-
-
-
-# Set up the display
-#screen = pygame.display.set_mode((400, 300))
-#pygame.display.set_caption("Timer Example")
-
-# Initialize font
-
-# Function to display the timer
-def show_timer(seconds):
-    font = pygame.font.Font(None, 75) # FONTUL TIMMERULUI
-    screen.fill((255, 255, 255))
-    text = font.render(str(seconds), True, (0, 0, 0))
-    text_rect = text.get_rect(center=(450, 450))
-    screen.blit(text, text_rect)
-    pygame.display.flip()
-
-# Timer function
-def timer():
-    for i in range(3, 0, -1):
-        show_timer(i)
-        pygame.time.wait(1000)
-
-    # Call your function here after the timer is finished
-
-
-# Set colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-BLUE = (0, 0, 255)
-
+# Boat dimensions
 boat_width_1 = 3 * cell_size
 boat_height_1 = 1 * cell_size
 
@@ -93,63 +44,91 @@ boat_height_2 = 1 * cell_size
 
 boat_width_3 = 5 * cell_size
 boat_height_3 = 1 * cell_size
-# Boat dimensions
 
 boat_width_4 = 2 * cell_size
 boat_height_4 = 1 * cell_size
 
+# Boats' initial positions
+boats = [
+    (2 * cell_size, 2 * cell_size),
+    (5 * cell_size, 4 * cell_size),
+    (1 * cell_size, 7 * cell_size),
+    (7 * cell_size, 1 * cell_size)
+]
 
-# Draw boats
-def draw_boats():
-    boat1_x, boat1_y = board_x + 2 * cell_size, board_y + 2 * cell_size
-    boat2_x, boat2_y = board_x + 5 * cell_size, board_y + 4 * cell_size
-    boat3_x, boat3_y = board_x + 1 * cell_size, board_y + 7 * cell_size
-    boat4_x, boat4_y = board_x + 7 * cell_size, board_y + 1 * cell_size
+# Drag and drop variables
+selected = None
+offset_x = 0
+offset_y = 0
 
-    pygame.draw.rect(screen, BLUE, (boat1_x, boat1_y, boat_width_1, boat_height_1))
-    pygame.draw.rect(screen, BLUE, (boat2_x, boat2_y, boat_width_2, boat_height_2))
-    pygame.draw.rect(screen, BLUE, (boat3_x, boat3_y, boat_width_3, boat_height_3))
-    pygame.draw.rect(screen, BLUE, (boat4_x, boat4_y, boat_width_4, boat_height_4))
+def show_timer(seconds):
+    font = pygame.font.Font(None, 75) # FONTUL TIMMERULUI
+    screen.fill((255, 255, 255))
+    text = font.render(str(seconds), True, (0, 0, 0))
+    text_rect = text.get_rect(center=(450, 450))
+    screen.blit(text, text_rect)
+    pygame.display.flip()
 
-# Your function to be called after the timer
-def your_function():
-    # Your function logic goes here
-    print("Your function is called after the timer.")
+# Timer func
+# tion
+def move_board2():
+    #move_board = True
+    #if move_board==False :
+    global board2_x
+    board2_x +=  15*(cell2_size-15)
+    #move_board=True
+# Main function to ru
+def timer():
+    for i in range(3, 0, -1):
+        show_timer(i)
+        pygame.time.wait(1000)
 
-# Call the timer function
-
+# Main function to run the game
 def run_game():
-    timer()
-    zz = False
+    global selected
+    global offset_x
+    global offset_y
+    timer();
+    move_board2()
     running = True
-    # if zz==False :
-    #draw_board()  # Draw the board
-    initial_state=True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                # conditie de iesire tre in while sa ii zic daca apesi pe x sau cv puton whatever si le fac butoane casutele
-        if initial_state==True:
-            print("a-")
-            screen.fill(Fundal)  # Fill the screen with Fundal
-            #draw_board()  # Draw the boards
-            #draw_board()  # Draw the board
-            draw_boats()  # Draw boats
-            #draw_board()  # Draw the boards
-            move_board2()  # Move the second board
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = event.pos
+                for i, (boat_x, boat_y) in enumerate(boats):
+                    if boat_x <= mouse_x <= boat_x + boat_width_1 and boat_y <= mouse_y <= boat_y + boat_height_1:
+                        selected = i
+                        offset_x = mouse_x - boat_x
+                        offset_y = mouse_y - boat_y
+            elif event.type == pygame.MOUSEBUTTONUP:
+                selected = None
+            elif event.type == pygame.MOUSEMOTION:
+                if selected is not None:
+                    mouse_x, mouse_y = event.pos
+                    boats[selected] = (mouse_x - offset_x, mouse_y - offset_y)
 
-            draw_board()  # Draw the boards
+        screen.fill(Fundal)  # Fill the screen with background color
+        draw_board()  # Draw the board
+        for i, (boat_x, boat_y) in enumerate(boats):
+            if i == selected:
+                color = (255, 0, 0)  # Highlight the selected boat in red
+            else:
+                color = (0, 0, 255)  # Set the default boat color to blue
+            if i == 0:
+                pygame.draw.rect(screen, color, (boat_x, boat_y, boat_width_1, boat_height_1))
+            elif i == 1:
+                pygame.draw.rect(screen, color, (boat_x, boat_y, boat_width_2, boat_height_2))
+            elif i == 2:
+                pygame.draw.rect(screen, color, (boat_x, boat_y, boat_width_3, boat_height_3))
+            elif i == 3:
+                pygame.draw.rect(screen, color, (boat_x, boat_y, boat_width_4, boat_height_4))
 
-            pygame.display.flip()  # Update the full display Surface to the screen
-            zz = True
-            initial_state=False
-                # de revazut de ce nu se modifica
-        pygame.display.flip()  # Update the full display Surface to the screen
+        pygame.display.flip()  # Update the full display surface
 
     pygame.quit()
 
 # Call the main function to run the game
 if __name__ == "__main__":
     run_game()
-
