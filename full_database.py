0ba954122f03e610ada2917e5e2d7e6c41290d49
Marius20031 <49350853+Marius20031@users.noreply.github.com>
2023-12-12 19:27:03 +0200

    hash+leaderboard
    Implemented hash on password+ leaderboard on matrix commited on main directly

--- full_database.py
+++ full_database.py
@@ -1,20 +1,11 @@
+import random
 from random import random
-import pygame
-from Importuri import *
-from Functii import *
-import pymongo
-from Importuri import *
-from pymongo import *
+import hashlib
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-from asyncio import sleep
-from random import random
-import pygame
-from Importuri import *
+
+from Functii import *
 from runde import *
-import numpy as np
-from importuri_bgd import *
-import random
 
 uri = "mongodb+srv://zenbot2020:KqVuBbkrJDuU18Ui@cluster5511.xyvvlgm.mongodb.net/?retryWrites=true&w=majority"
 #uri = "mongodb+srv://bodo2003:eoXq7eK98PQyfwsm@cluster0.facrcqq.mongodb.net/?retryWrites=true&w=majority"
@@ -23,16 +14,33 @@ uri = "mongodb+srv://zenbot2020:KqVuBbkrJDuU18Ui@cluster5511.xyvvlgm.mongodb.net
 client = MongoClient(uri, server_api=ServerApi('1'))
 db = client["Datapygame"]
 collection = db["Collection"]
+def get_leaderboard():
+    nr_playeri[0]=collection.count_documents({})
+    var_documente=collection.find({}).sort("xp", -1)
+    for document in var_documente:
+        username = document.get("username")
+        xp = document.get("xp")
+        level =int(xp/10)
+
+        matrice_leaderboard.append([username, xp, level])
+    print(matrice_leaderboard)
+
+
 # astea le vreau mereu ^
+def calculate_sha256(input_string):
+    sha256_hash = hashlib.sha256(input_string.encode()).hexdigest()
+    return sha256_hash
+
 def random_guest_name():
     var=random.randint(123,912)
     var+=87
     return("Guest"+str(var))
 
 def verificare_exisa(username,parola):
+    #hashed_password = calculate_sha256(parola)
     query = {
         'username': username,
-        'password': parola
+        'password_hash': parola
     }
 
     var=collection.find_one(query)
@@ -53,10 +61,11 @@ def verificare_exista_username(username):
         return 1
     else:
         return 0
-
 def login_try(username,password):
     #verific daca exista prima data, si daca nu exista inserez
-    if verificare_exisa(username,password)==1:
+    hashed_password = calculate_sha256(password)
+    if verificare_exisa(username,hashed_password)==1:
+        get_leaderboard()
         print("Login corect, datele sunt corecte")
         return 0
     else: # creeaza
@@ -69,11 +78,12 @@ def create_try(username,password):
         print("Username deja folosit")
         return 1
     else: # creeaza
+        hashed_password = calculate_sha256(password)
         user_id = 1 # ce alege
         xp = 0
         data_to_insert = {
             'username': username,
-            'password': password,
+            'password_hash': hashed_password,
             'id': user_id,
             'xp': xp
         }
@@ -106,4 +116,5 @@ def adauga_xp_ratare():
     xp+=5
     collection.update_one(query, {'$set': {'xp': xp}})
     print("ai ratat")
+
 #def level:
